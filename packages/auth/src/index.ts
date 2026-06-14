8715988736bf82c54d952054af4aa9834a3dc7ec
jojomensah89 <jojomensah89@gmail.com>
2026-06-14 14:58:47 +0000

    Add GitHub workflows for deployment to Dev and Prod environments, and enable session cookie caching in createAuth function

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -17,13 +17,12 @@ export function createAuth() {
     emailAndPassword: {
       enabled: true,
     },
-    // uncomment cookieCache setting when ready to deploy to Cloudflare using *.workers.dev domains
-    // session: {
-    //   cookieCache: {
-    //     enabled: true,
-    //     maxAge: 60,
-    //   },
-    // },
+    session: {
+      cookieCache: {
+        enabled: true,
+        maxAge: 5 * 60,
+      },
+    },
     secret: env.BETTER_AUTH_SECRET,
     baseURL: env.BETTER_AUTH_URL,
     advanced: {
@@ -32,12 +31,9 @@ export function createAuth() {
         secure: true,
         httpOnly: true,
       },
-      // uncomment crossSubDomainCookies setting when ready to deploy and replace <your-workers-subdomain> with your actual workers subdomain
-      // https://developers.cloudflare.com/workers/wrangler/configuration/#workersdev
-      // crossSubDomainCookies: {
-      //   enabled: true,
-      //   domain: "<your-workers-subdomain>",
-      // },
+      crossSubDomainCookies: env.COOKIE_DOMAIN
+        ? { enabled: true, domain: env.COOKIE_DOMAIN }
+        : undefined,
     },
   });
 }
